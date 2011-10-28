7fdba16d1c278f036c198f736d61e422520efca7
noRerih <kaupanin@gmail.com>
2011-10-28 10:19:15 +0400

    options: timeout and callbacks

--- sisyphus.js
+++ sisyphus.js
@@ -7,27 +7,40 @@
   
 (function($) {
   
-  $.fn.sisyphus = function() {
-    protect(this);
+  $.fn.sisyphus = function(options) {
+    options = $.extend($.fn.sisyphus.defaults, options);
+    protect(this, options);
   };
+  
+  $.fn.sisyphus.defaults = {
+    excludeFields: null,
+    customKeyPrefix: '',
+    timeout: 0,
+    onSaveCallback: function() {},
+    onRestoreCallback: function() {},
+    onReleaseDataCallback: function() {}
+  }
   
   /**
    * Protect specified forms, store it's fields data to local storage and restore them on page load
    *
-   * @param [Object] targets  - forms object(s), result of jQuery selector
+   * @param [Object] targets    forms object(s), result of jQuery selector
+   * @param Object options      plugin options
+   *
    * @return void
    */
-  function protect(targets) {
+  function protect(targets, options) {
     if (! isLocalStorageAvailable()) {
       return false;
     }
-    
+    console.log(options);
+
     var href = window.location.hostname + window.location.pathname + window.location.search;
     $(targets).each(function(){
       var target = $(this);
       var protectedFields = target.find(':input');
-      bindSaveAndRestoreData(protectedFields, target, href);
-      bindReleaseDataOnSubmitOrReset(protectedFields, target, href);
+      bindSaveAndRestoreData(protectedFields, target, href, options);
+      bindReleaseDataOnSubmitOrReset(protectedFields, target, href, options);
     });
   }
   
@@ -38,18 +51,24 @@
    * @param Object protectedFields   jQuery object contains form fields to protect
    * @param Object target            jQuery target form object
    * @param String href              current window location
+   * @param Object options           plugin options
    *
    * @return void
    */
-  function bindSaveAndRestoreData(protectedFields, target, href) {
+  function bindSaveAndRestoreData(protectedFields, target, href, options) {
+    var restored = false;
     protectedFields.each(function(){
-      var prefix = href + target[0].id + this.name;
+      var prefix = href + target[0].id + this.name + options.customKeyPrefix;
       var resque = localStorage.getItem(prefix);
       if (resque) {
-        restoreData(this, resque)
+        restoreData(this, resque, options);
+        restored = true;
       }
-      bindSaveData(this, prefix);
+      bindSaveData(this, prefix, options);
     });
+    if (restored && typeof options.onRestoreCallback == 'function') {
+      options.onRestoreCallback.call();
+    }
   }
   
   
@@ -58,10 +77,11 @@
    *
    * @param HTMLDomElement elem   form element object
    * @param String resque         previously stored fields data
+   * @param Object options        plugin options
    *
    * @return void
    */
-  function restoreData(elem, resque) {
+  function restoreData(elem, resque, options) {
     if (elem.type == 'checkbox' && resque != 'false' && elem.name.indexOf('[') == -1) {
       elem.checked = true;
     } else if (elem.type == 'radio') {
@@ -82,37 +102,112 @@
    *
    * @param HTMLDomElement elem   form element object
    * @param String prefix         prefix used as key to store data in local storage
+   * @param Object options        plugin options
    *
    * @return void
    */
-  function bindSaveData(elem, prefix) {
+  function bindSaveData(elem, prefix, options) {
+    if ($(elem).is(':text') || $(elem).is('textarea')) {
+      if (! options.timeout) {
+        bindSaveDataImmediately(elem, prefix, options);
+      } else {
+        bindSaveDataByTimeout(elem, prefix, options);
+      }
+    } else {
+      bindSaveDataOnChange(elem, prefix, options);
+    }
+  }
+  
+  
+  /**
+   * Bind immediate saving (on typing/checking/changing) field data to local storage when user fills it
+   *
+   * @param HTMLDomElement elem   form element object
+   * @param String prefix         prefix used as key to store data in local storage
+   * @param Object options        plugin options
+   *
+   * @return void
+   */
+  function bindSaveDataImmediately(elem, prefix, options) {
     if ($(elem).is(':text') || $(elem).is('textarea')) {
       elem.oninput = function() {
         try {
           localStorage.setItem(prefix, elem.value);
-        } catch (e) {
-          //QUOTA_EXCEEDED_ERR
+        } catch (e) { /*QUOTA_EXCEEDED_ERR*/ }
+        if (typeof options.onSaveCallback == 'function') {
+          options.onSaveCallback.call();
         }
       }
-    } else {
-      elem.onchange = function() {
-        var value = $(elem).val();
-        if (elem.type == 'checkbox') {
-          if (elem.name.indexOf('[') != -1) {
-            value = [];
-            $('[name="' + elem.name + '"]:checked').each(function(){ value.push(this.value) });
-          } else {
-            value = $(elem).is(':checked');
+    }
+  }
+  
+  
+  /**
+   * Bind saving (by timeout) field data to local storage when user fills it
+   *
+   * @param HTMLDomElement elem   form element object
+   * @param String prefix         prefix used as key to store data in local storage
+   * @param Object options        plugin options
+   *
+   * @return void
+   */
+  function bindSaveDataByTimeout(elem, prefix, options) {
+    if ($(elem).is(':text') || $(elem).is('textarea')) {
+      setTimeout((function(elem){
+        function timeout(){
+          try {
+            localStorage.setItem(prefix, elem.value);
+          } catch (e) {  }
+          if (typeof options.onSaveCallback == 'function') {
+            options.onSaveCallback.call();
           }
+          setTimeout(timeout, options.timeout * 1000);
         }
-        if (elem.type == 'radio') {
-          value = elem.value;
+        return timeout;
+      })(elem), options.timeout * 1000);
+      /*(function(elem){
+        (function timeout(){
+          try {
+            localStorage.setItem(prefix, elem.value);
+          } catch (e) {  }
+          if (typeof options.onSaveCallback == 'function') {
+            options.onSaveCallback.call();
+          }
+          setTimeout(timeout, options.timeout * 1000);
+        })();
+      })(elem);*/
+    }
+  }
+  
+  
+  /**
+   * Bind saving field data on change
+   *
+   * @param HTMLDomElement elem   form element object
+   * @param String prefix         prefix used as key to store data in local storage
+   * @param Object options        plugin options
+   *
+   * @return void
+   */
+  function bindSaveDataOnChange(elem, prefix, options) {
+    elem.onchange = function() {
+      var value = $(elem).val();
+      if (elem.type == 'checkbox') {
+        if (elem.name.indexOf('[') != -1) {
+          value = [];
+          $('[name="' + elem.name + '"]:checked').each(function(){ value.push(this.value) });
+        } else {
+          value = $(elem).is(':checked');
         }
-        try {
-          localStorage.setItem(prefix, value);
-        } catch (e) {
-          //QUOTA_EXCEEDED_ERR
-        }
+      }
+      if (elem.type == 'radio') {
+        value = elem.value;
+      }
+      try {
+        localStorage.setItem(prefix, value);
+      } catch (e) { /*QUOTA_EXCEEDED_ERR*/ }
+      if (typeof options.onSaveCallback == 'function') {
+        options.onSaveCallback.call();
       }
     }
   }
@@ -124,15 +219,16 @@
    * @param Object protectedFields   jQuery object contains form fields to protect
    * @param Object target            jQuery target form object
    * @param String href              current window location
+   * @param Object options           plugin options
    *
    * @return void
    */
-  function bindReleaseDataOnSubmitOrReset(protectedFields, target, href) {
+  function bindReleaseDataOnSubmitOrReset(protectedFields, target, href, options) {
     target.submit(function(){
-      releaseData(protectedFields, target, href);
+      releaseData(protectedFields, target, href, options);
     });
     target.bind('reset', function(){
-      releaseData(protectedFields, target, href);
+      releaseData(protectedFields, target, href, options);
     });
   }
   
@@ -143,14 +239,20 @@
    * @param Object protectedFields   jQuery object contains form fields to protect
    * @param Object target            jQuery target form object
    * @param String href              current window location
+   * @param Object options           plugin options
    *
    * @return void
    */
-  function releaseData(protectedFields, target, href) {
+  function releaseData(protectedFields, target, href, options) {
+    var released = false;
     protectedFields.each(function(){
       var prefix = href + target[0].id + this.name;
       localStorage.removeItem(prefix);
+      released = true;
     });
+    if (released && typeof options.onReleaseDataCallback == 'function') {
+      options.onReleaseDataCallback.call();
+    }
   }
   
   
