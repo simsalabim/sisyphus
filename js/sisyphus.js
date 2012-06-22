e0a27fba66bc15b08f5465c566ffa04293946d3c
Alexander Kaupanin <kaupanin@gmail.com>
2012-06-22 16:33:18 +0400

    Sisyphus updated

--- js/sisyphus.js
+++ js/sisyphus.js
@@ -4,7 +4,7 @@
  *
  * @author Alexander Kaupanin <kaupanin@gmail.com>
  */
-	
+
 //$.sisyphus().setOptions({timeout: 15})
 ( function( $ ) {
 	$.sisyphus = function() {
@@ -13,79 +13,79 @@
 
 	$.fn.sisyphus = function( options ) {
 		var sisyphus = Sisyphus.getInstance();
-		sisyphus.setOptions( options )
+		sisyphus.setOptions( options );
 		sisyphus.protect( this );
 		return sisyphus;
 	};
 
-  var browserStorage = {};
+	var browserStorage = {};
 
- /**
-   * Check if local storage or other browser storage is available
-   *
-   * @return Boolean
-   */
-  browserStorage.isAvailable = function() {
-    if ( typeof $.jStorage === "object" ) {
-      return true;
-    }
-    try {
-      return localStorage.getItem;
-    } catch ( e ) {
-        return false;
-    }
-  }
+	/**
+	 * Check if local storage or other browser storage is available
+	 *
+	 * @return Boolean
+	 */
+	browserStorage.isAvailable = function() {
+		if ( typeof $.jStorage === "object" ) {
+			return true;
+		}
+		try {
+			return localStorage.getItem;
+		} catch ( e ) {
+			return false;
+		}
+	};
 
- /**
-   * Set data to browser storage
-   *
-   * @param [String] key
-   * @param [String] value
-   *
-   * @return Boolean
-   */
-  browserStorage.set = function( key, value ) {
-    if ( typeof $.jStorage === "object" ) {
-      $.jStorage.set( key, value + "" )
-    } else {
-      try {
-        localStorage.setItem( key, value + "" );
-      } catch (e) {
-        //QUOTA_EXCEEDED_ERR
-      }
-    }
-  }
+	/**
+	 * Set data to browser storage
+	 *
+	 * @param [String] key
+	 * @param [String] value
+	 *
+	 * @return Boolean
+	 */
+	browserStorage.set = function( key, value ) {
+		if ( typeof $.jStorage === "object" ) {
+			$.jStorage.set( key, value + "" );
+		} else {
+			try {
+				localStorage.setItem( key, value + "" );
+			} catch (e) {
+				//QUOTA_EXCEEDED_ERR
+			}
+		}
+	};
 
-  /**
-   * Get data from browser storage by specified key
-   *
-   * @param [String] key
-   *
-   * @return string
-   */
-  browserStorage.get = function( key ) {
-    if ( typeof $.jStorage === "object" ) {
-      var result = $.jStorage.get( key );
-      return result ? result.toString() : result;
-    } else {
-      return localStorage.getItem( key )
-    }
-  }
+	/**
+	 * Get data from browser storage by specified key
+	 *
+	 * @param [String] key
+	 *
+	 * @return string
+	 */
+	browserStorage.get = function( key ) {
+		if ( typeof $.jStorage === "object" ) {
+			var result = $.jStorage.get( key );
+			return result ? result.toString() : result;
+		} else {
+			return localStorage.getItem( key );
+		}
+	};
 
-  /**
-   * Delete data from browser storage by specified key
-   *
-   * @param [String] key
-   *
-   * @return void
-   */
-  browserStorage.remove = function( key ) {
-    if ( typeof $.jStorage === "object" ) {
-      $.jStorage.deleteKey( key );
-    } else {
-      localStorage.removeItem( key );
-    }
-  }
+	/**
+	 * Delete data from browser storage by specified key
+	 *
+	 * @param [String] key
+	 *
+	 * @return void
+	 */
+	browserStorage.remove = function( key ) {
+		if ( typeof $.jStorage === "object" ) {
+			$.jStorage.deleteKey( key );
+		} else {
+			localStorage.removeItem( key );
+		}
+	};
 
 	Sisyphus = ( function() {
 		var params = {
@@ -94,7 +94,7 @@
 		};
 
 		function init () {
-		
+
 			return {
 				/**
 				 * Set plugin initial options
@@ -105,7 +105,7 @@
 				 */
 				setInitialOptions: function ( options ) {
 					var defaults = {
-						excludeFields: null,
+						excludeFields: [],
 						customKeyPrefix: "",
 						timeout: 0,
 						autoRelease: true,
@@ -118,7 +118,7 @@
 					this.options = this.options || $.extend( defaults, options );
 					this.browserStorage = browserStorage;
 				},
-			
+
 				/**
 				 * Set plugin options
 				 *
@@ -129,9 +129,8 @@
 				setOptions: function ( options ) {
 					this.options = this.options || this.setInitialOptions( options );
 					this.options = $.extend( this.options, options );
-				}, 
-			
-			
+				},
+
 				/**
 				 * Protect specified forms, store it's fields data to local storage and restore them on page load
 				 *
@@ -145,14 +144,14 @@
 					var self = this;
 					this.targets = this.targets || [];
 					this.href = this.options.name || location.hostname + location.pathname + location.search + location.hash;
-				
+
 					this.targets = $.merge( this.targets, targets );
 					this.targets = $.unique( this.targets );
 					this.targets = $( this.targets );
 					if ( ! this.browserStorage.isAvailable() ) {
 						return false;
 					}
-				
+
 					self.restoreAllData();
 					if ( this.options.autoRelease ) {
 						self.bindReleaseData();
@@ -162,11 +161,7 @@
 						params.started = true;
 					}
 				},
-			
-			
 
-
-			
 				/**
 				 * Bind saving data
 				 *
@@ -174,15 +169,15 @@
 				 */
 				bindSaveData: function() {
 					var self = this;
-				
+
 					if ( self.options.timeout ) {
 						self.saveDataByTimeout();
 					}
-				
+
 					self.targets.each( function() {
 						var targetFormId = $( this ).attr( "id" );
 						var fieldsToProtect = $( this ).find( ":input" ).not( ":submit" ).not( ":reset" ).not( ":button" ).not( ":file" );
-						
+
 						fieldsToProtect.each( function() {
 							if ( $.inArray( this, self.options.excludeFields ) !== -1 ) {
 								// Returning non-false is the same as a continue statement in a for loop; it will skip immediately to the next iteration.
@@ -198,10 +193,9 @@
 								self.bindSaveDataOnChange( field, prefix );
 							}
 						} );
-					} )
+					} );
 				},
-			
-			
+
 				/**
 				 * Save all protected forms data to Local Storage.
 				 * Common method, necessary to not lead astray user firing 'data are saved' when select/checkbox/radio
@@ -217,8 +211,8 @@
 
 						fieldsToProtect.each( function() {
 							var field = $( this );
-							// Returning non-false is the same as a continue statement in a for loop; it will skip immediately to the next iteration.
 							if ( $.inArray( this, self.options.excludeFields ) !== -1 || field.attr( "name" ) === undefined ) {
+								// Returning non-false is the same as a continue statement in a for loop; it will skip immediately to the next iteration.
 								return true;
 							}
 							var prefix = self.href + targetFormId + field.attr( "name" ) + self.options.customKeyPrefix;
@@ -248,8 +242,7 @@
 						self.options.onSave.call();
 					}
 				},
-			
-			
+
 				/**
 				 * Restore forms data from Local Storage
 				 *
@@ -258,17 +251,18 @@
 				restoreAllData: function() {
 					var self = this;
 					var restored = false;
-					
+
 					if ( $.isFunction( self.options.onBeforeRestore ) ) {
 						self.options.onBeforeRestore.call();
 					}
-					
+
 					self.targets.each( function() {
 						var target = $( this );
 						var targetFormId = target.attr( "id" );
 						var fieldsToProtect = target.find( ":input" ).not( ":submit" ).not( ":reset" ).not( ":button" ).not( ":file" );
-						
+
 						fieldsToProtect.each( function() {
+
 							if ( $.inArray( this, self.options.excludeFields ) !== -1 ) {
 								// Returning non-false is the same as a continue statement in a for loop; it will skip immediately to the next iteration.
 								return true;
@@ -282,13 +276,12 @@
 							}
 						} );
 					} );
-				
+
 					if ( restored && $.isFunction( self.options.onRestore ) ) {
 						self.options.onRestore.call();
 					}
 				},
-			
-			
+
 				/**
 				 * Restore form field data from local storage
 				 *
@@ -308,14 +301,13 @@
 							field.attr( "checked", "checked" );
 						}
 					} else if ( field.attr( "name" ).indexOf( "[" ) === -1 ) {
-						field.val( resque ); 
+						field.val( resque );
 					} else {
 						resque = resque.split( "," );
 						field.val( resque );
 					}
 				},
-			
-			
+
 				/**
 				 * Bind immediate saving (on typing/checking/changing) field data to local storage when user fills it
 				 *
@@ -326,18 +318,17 @@
 				 */
 				bindSaveDataImmediately: function( field, prefix ) {
 					var self = this;
-					if ( $.browser.msie == null ) {
+					if ( $.browser.msie === null ) {
 						field.get(0).oninput = function() {
 							self.saveToBrowserStorage( prefix, field.val() );
-						}
+						};
 					} else {
 						field.get(0).onpropertychange = function() {
 							self.saveToBrowserStorage( prefix, field.val() );
-						}
+						};
 					}
 				},
-			
-			
+
 				/**
 				 * Save data to Local Storage and fire callback if defined
 				 *
@@ -349,14 +340,13 @@
 				 */
 				saveToBrowserStorage: function( key, value, fireCallback ) {
 					// if fireCallback is undefined it should be true
-					fireCallback = fireCallback == null ? true : fireCallback;
+					fireCallback = fireCallback === null ? true : fireCallback;
 					this.browserStorage.set( key, value );
 					if ( fireCallback && value !== "" && $.isFunction( this.options.onSave ) ) {
 						this.options.onSave.call();
 					}
 				},
-			
-			
+
 				/**
 				 * Bind saving field data on change
 				 *
@@ -371,8 +361,7 @@
 						self.saveAllData();
 					} );
 				},
-			
-			
+
 				/**
 				 * Saving (by timeout) field data to local storage when user fills it
 				 *
@@ -389,8 +378,7 @@
 						return timeout;
 					} )( targetForms ), self.options.timeout * 1000 );
 				},
-			
-			
+
 				/**
 				 * Bind release form fields data from local storage on submit/reset form
 				 *
@@ -404,10 +392,8 @@
 						var formId = target.attr( "id" );
 						$( this ).bind( "submit reset", function() {
 							self.releaseData( formId, fieldsToProtect );
-						} )
-					} )
-				
-				
+						} );
+					} );
 				},
 
 				/**
@@ -422,7 +408,7 @@
 						var fieldsToProtect = target.find( ":input" ).not( ":submit" ).not( ":reset" ).not( ":button" ).not( ":file" );
 						var formId = target.attr( "id" );
 						self.releaseData( formId, fieldsToProtect );
-					} )
+					} );
 				},
 
 				/**
@@ -443,27 +429,27 @@
 						}
 						var field = $( this );
 						var prefix = self.href + targetFormId + field.attr( "name" ) + self.options.customKeyPrefix;
-						self.browserStorage.remove( prefix )
+						self.browserStorage.remove( prefix );
 						released = true;
 					} );
-				
+
 					if ( released && $.isFunction( self.options.onRelease ) ) {
 						self.options.onRelease.call();
 					}
 				}
-			
+
 			};
 		}
-	
+
 		return {
 			getInstance: function() {
 				if ( ! params.instantiated ) {
 					params.instantiated = init();
 					params.instantiated.setInitialOptions();
 				}
-				return params.instantiated; 
+				return params.instantiated;
 			},
-		
+
 			free: function() {
 				params = {};
 				return null;
