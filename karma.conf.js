ef14f61e8e993c8f199db372aced5710326d756f
Alexander Kaupanin <kaupanin@gmail.com>
2014-10-04 11:20:15 -0500

    Configured `npm test` and added .travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
